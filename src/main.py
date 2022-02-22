5328cf811583d8171c8ceb0d6df173f05db9d85b
root <root@localhost>
2022-02-22 07:28:09 +0100

    auto publish 1645511289530

--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
-def check_num(num):
-  order = len(str(num))
-  sum = 0
-  temp = num
-  while temp >0:
-    digit = temp% 10
-    sum += digit**order
+def check_armstrong(num):
+    order = len(str(num))
     
+    sum = 0
+    
+    # use temp to find each digit. Then power it
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
     return num == sum
-num = int(input("Enter num "))
-if check_num(num):
-  print(num,"is an amstrong number")
+ 
+num = int(input('Enter your number: '))
+ 
+if check_armstrong(num):
+    print(num,"is an Armstrong number")
 else:
-  print(num,"is not an amstrong number")
-
-  
-
-  
+    print(num,"is not an Armstrong number")
