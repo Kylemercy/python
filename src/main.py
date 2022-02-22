0e79972ef390b24885635bf3986d2df4a3b49643
root <root@localhost>
2022-02-22 07:27:17 +0100

    auto publish 1645511236823

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print(5 + 3)
+def check_num(num):
+  order = len(str(num))
+  sum = 0
+  temp = num
+  while temp >0:
+    digit = temp% 10
+    sum += digit**order
+    
+    return num == sum
+num = int(input("Enter num "))
+if check_num(num):
+  print(num,"is an amstrong number")
+else:
+  print(num,"is not an amstrong number")
+
+  
+
+  
