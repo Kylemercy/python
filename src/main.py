6d6d07560fcc7bb48e26746fd8f464ee641520c2
root <root@localhost>
2022-02-27 22:02:19 +0100

    auto publish 1645995738975

--- src/main.py
+++ src/main.py
@@ -1,17 +1,36 @@
-def computer_gcd(x,y):
-  small = min(x,y)
-  gcd = 1
-  for i in range(1, small +1):
-    if((x%i == 0) and(y% i==0)):
-      gcd == i
-  return gcd
-num1 = int (input("Enter NUM 1: "))
-num2 =int(input("Enter NUM 2: "))
-gcd = computer_gcd(num1,num2)
-print("The gcd of",num1,"and",num2,"is",gcd)
-    
-  
-    
-  
+from random import randint
 
- 
+#create a list of play options
+t = ["Rock", "Paper", "Scissors"]
+
+#assign a random play to the computer
+computer = t[randint(0,2)]
+
+#set player to False
+player = False
+
+while player == False:
+#set player to True
+    player = input("Rock, Paper, Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+    #player was set to True, but we want it to be False so the loop continues
+    player = False
+    computer = t[randint(0,2)]
