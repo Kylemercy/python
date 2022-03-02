ee76bda9732670941434d8e881dcb4988ae6a6a8
root <root@localhost>
2022-03-02 13:35:33 +0100

    auto publish 1646224533728

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
-odds = {1,3,5,7,9}
-evens = {0,2,4,6,8}
-prime = {1,2,5,7,}
-print(odds.union(evens,prime))
-print(odds|(evens))
-#you can either use union or|
-print(odds.intersection(evens,))
-# this gives an empty set
-print(odds.intersection(prime))
-print(evens.intersection(prime))
+num1 = {1,5,7,6,3,8,2}
+num2 = {1,5,2,11,15}
+diff = num1.difference(num2)
+print(diff)
+# this print the number that is in num1
+#but not in num2
+diff2 = num2.difference(num1)
+print(diff2)
+#this also print numberin num2 but not
+#in num1
+seta = num2.symmetric_difference(num1)
+print(seta)
+#this print all the numbers that are not found in
+# num1 and num2
+# that it combines their differences
+num1.update(num2)
+print(num1)
