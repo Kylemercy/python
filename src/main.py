40f4c9247d1e78b6b0d8bcd6b5305513eba8b8a9
root <root@localhost>
2022-03-02 13:17:50 +0100

    auto publish 1646223470654

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
-my_dict = {"name":"mercy","age": 34, "country":"china"}
-for key in my_dict.keys():
-  print(key)
+odds = {1,3,5,7,9}
+evens = {0,2,4,6,8}
+prime = {1,2,5,7,}
+print(odds.union(evens,prime))
+print(odds|(evens))
+#you can either use union or|
+print(odds.intersection(evens,))
+# this gives an empty set
+print(odds.intersection(prime))
+print(evens.intersection(prime))
