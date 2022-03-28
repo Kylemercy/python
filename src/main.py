285fb1dcdbc3d83b3b34acb66e0f253444f3f5f0
root <root@localhost>
2022-03-28 13:54:40 +0100

    auto publish 1648472080398

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-d = {'foo': 1, 'bar': 2, 'baz': 3}
-print(d.values())
-print(d.items())
-d = {'foo': 1, 'bar': 2, 'baz': 3}
-for k, v in d.items():
- print('k =', k, ', v =', v)
-for k,v in d.items():
-  print(k,v)
+
+x = int(input())
+if x % 2 == 0: 
+  print(x, " is even.")
+  print("Did you know that 2 is the only even number that is prime?")
+else:
+  print(x, " is odd.") 
+  print("Did you know that multiplying two odd numbers " + "always gives an odd result?")
+import math
+import random
+y = int(input("Enter a number: "))
+if y <= 0:
+  print("sorry negative number is not allowed")
+  print("i will use the default number in range 1-10 ")
+  y = random.randint(1,10+1)
+  
+mit =math.sqrt(y)
+print(  "the square root of ", y ,"is", round(mit,2))
