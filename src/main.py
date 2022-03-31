10ddd7d26b138ee41e67b42df198ce9ca3b21019
root <root@localhost>
2022-03-31 11:46:20 +0100

    auto publish 1648723580253

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-def final_amount(p,n,r,t):
-  a = p*(1 + r//n)**(n*t)
-  return a
-num = float(input("Enter the amount you want to invest: "))
-total = final_amount(num,0.07,9,5)
-total = round(total,2)
-print(f"At the end of the given time,you will have  ${total}")
+ix = 0 
+fruit = input("name a fruit: ")
+while ix < len(fruit):
+ letter = fruit[ix]
+ print(letter)
+ ix += 1
+prefixes = "JKLMNOPQ"
+suffix = "ack" 
+for p in prefixes: 
+  print(p + suffix)
