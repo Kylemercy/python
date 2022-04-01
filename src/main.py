d7381a78eb10cbc03a75a38c9e149c124cac1a8a
root <root@localhost>
2022-04-01 17:00:17 +0100

    auto publish 1648828817363

--- src/main.py
+++ src/main.py
@@ -1,54 +1,27 @@
-class Robot:
-    """Represents a robot, with a name."""
+class employee:
+  raise_amount = 1.09
+  def __init__(self,first,last,pay):
+   
+    self.first = first
+    self.last = last
+    self.pay = pay
+    self.email = self.first+ " " + self.last + " @gamil.company"
+  
+  def full_name(self):
+    return self.first + " " + self.last
+    return self . email
 
-    # A class variable, counting the number of robots
-    population = 0
+  def apply_raise(self):
+    return self.pay * self.raise_amount
+  
+  
 
-    def __init__(self, name):
-        """Initializes the data."""
-        self.name = name
-        print(f"initializing{self.name}")
+result = employee("mercy","Jhon",45000)
+result1 = employee("chika","Matthew",59000)
+result.raise_amount = 2  
+print(result1.full_name())
+print(result.pay)
+print(result.apply_raise())
+print(result.__dict__)
+  
 
-        # When this person is created, the robot
-        # adds to the population
-        Robot.population += 1
-
-    def die(self):
-        """I am dying."""
-        print("{} is being destroyed!".format(self.name))
-
-        Robot.population -= 1
-
-        if Robot.population == 0:
-            print("{} was the last one.".format(self.name))
-        else:
-            print("There are still {:d} robots working.".format(
-                Robot.population))
-
-    def say_hi(self):
-        """Greeting by the robot.
-
-        Yeah, they can do that."""
-        print("Greetings, my masters call me {}.".format(self.name))
-
-    @classmethod
-    def how_many(cls):
-        """Prints the current population."""
-        print("We have {:d} robots.".format(cls.population))
-
-
-droid1 = Robot("R2-D2")
-droid1.say_hi()
-Robot.how_many()
-
-droid2 = Robot("C-3PO")
-droid2.say_hi()
-Robot.how_many()
-
-print("\nRobots can do some work here.\n")
-
-print("Robots have finished their work. So let's destroy them.")
-droid1.die()
-droid2.die()
-
-Robot.how_many()
