e326cee9fdad427836482269c333edd354d4d1f8
root <root@localhost>
2022-04-01 07:30:19 +0100

    auto publish 1648794619365

--- src/main.py
+++ src/main.py
@@ -1,9 +1,54 @@
-name = "mercy","joy","sandra"
-age = 12,14,18
-ini = "me","jy","sd"
-txt = zip(name,age,ini)
+class Robot:
+    """Represents a robot, with a name."""
 
-print(list(txt))
-print (tuple(txt))
-for item in txt:
-  print(list(item))
+    # A class variable, counting the number of robots
+    population = 0
+
+    def __init__(self, name):
+        """Initializes the data."""
+        self.name = name
+        print(f"initializing{self.name}")
+
+        # When this person is created, the robot
+        # adds to the population
+        Robot.population += 1
+
+    def die(self):
+        """I am dying."""
+        print("{} is being destroyed!".format(self.name))
+
+        Robot.population -= 1
+
+        if Robot.population == 0:
+            print("{} was the last one.".format(self.name))
+        else:
+            print("There are still {:d} robots working.".format(
+                Robot.population))
+
+    def say_hi(self):
+        """Greeting by the robot.
+
+        Yeah, they can do that."""
+        print("Greetings, my masters call me {}.".format(self.name))
+
+    @classmethod
+    def how_many(cls):
+        """Prints the current population."""
+        print("We have {:d} robots.".format(cls.population))
+
+
+droid1 = Robot("R2-D2")
+droid1.say_hi()
+Robot.how_many()
+
+droid2 = Robot("C-3PO")
+droid2.say_hi()
+Robot.how_many()
+
+print("\nRobots can do some work here.\n")
+
+print("Robots have finished their work. So let's destroy them.")
+droid1.die()
+droid2.die()
+
+Robot.how_many()
