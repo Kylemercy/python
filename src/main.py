5505b2b174791c2ac32a9970eca20f59f039721a
root <root@localhost>
2022-04-02 16:57:21 +0100

    auto publish 1648915041175

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
-class employee:
+class Employee:
   num_emp = 0
 
-  raise_amount = 1.09
+  raise_amount = 1.74
   def __init__(self,first,last,pay):
    
     self.first = first
     self.last = last
     self.pay = pay
     self.email = self.first+ " " + self.last + " @gamil.company"
-    employee.num_emp += 1
+    Employee.num_emp += 1
   def full_name(self):
     return self.first + " " + self.last
-    return self . email
+    
 
   def apply_raise(self):
     return self.pay * self.raise_amount
@@ -32,10 +32,46 @@ class employee:
        return " its weekend enjoy your weekend"
      else:
        return " its a workday be productive"
+class developer(Employee):
+  raise_amount = 1.30
+  def __init__(self,first,last,pay,pro_lang):
+    super().__init__(first,last,pay)
+    # you can use the first option or second
+    #Employee.__init__(self, first, last,pay)
+    self.pro_lang = pro_lang
 
-import datetime
-my_work = datetime.date(2022,3,10)
-print(employee.workday(my_work))
+class Manager(Employee):
+  def __init__(self,first,last, pay,employees = None):
+    super().__init__(first,last,pay)
+    if employees is None:
+      self.employees = []
+    else:
+     self.employees = employees
+  def add_emp(self,emp):
+    if emp not in self.employees:
+      self.employees.append(emp)
+      
+      
+  def remove_emp(self,emp):
+    if emp in self.employees:
+      self.employees.remove(emp)
+    
+  def print_emp(self):
+    for emp in self.employees:
+      print(">>>",emp.full_name())  
+  
+
+dv1 = developer("mercy","jang",5000,"Python")
+dv2 = developer("kate","san",6000,"java")
+mg1 = Manager("sie","dan",6900,[dv1])
+print(mg1.email)
+print(mg1.full_name())
+print(mg1.add_emp(dv2))
+print(mg1.print_emp())
+
+
+
+
      
      
    
