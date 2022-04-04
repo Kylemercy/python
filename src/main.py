3caa6b276a9ca6bf0a9e63e7db11153a794740ed
root <root@localhost>
2022-04-04 09:44:46 +0100

    auto publish 1649061886442

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 hotel = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-room = [ ] 
-taken = 20
+room = [ ]
+taken = 5
+
 print("*********************************")
 print("welcome to hotel costal Rings!")
 print("*********************************")
@@ -9,24 +10,40 @@ print("###############################")
 print("the are 20 rooms available")
 print("###############################")
 
-while taken > 1:
+while taken >= 1 :
    choice = int(input("Enter the room number of your choice: ")) 
+
+   
    if choice not in hotel:
      print("room number is don't exists")
    
    elif choice in room:
      print("sorry room is taken already")
      print("please choose another room")
-     
-   
+  
    else:
      room.append(choice)
      print("welcome Room of choice is available")
      print("Here is your room key") 
    taken -= 1
-print("\n")
-print("Room is full \nwe are sorry our room is currently booked") 
    
+  
+print("\n")
+print("checking in for available rooms")
+
+for i in hotel:  
+    mit = [ ] 
+    if i not in room:
+      mit.append(i)
+      print((mit),end = " ")
+    
+else:
+  print("\n")
+  print("This are the available rooms remaining")
+    
+
+
+
    
      
    
