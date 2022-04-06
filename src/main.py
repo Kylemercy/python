8ec913aac9ee0f0716b4626ef945e893da877e91
root <root@localhost>
2022-04-06 18:15:09 +0100

    auto publish 1649265309133

--- src/main.py
+++ src/main.py
@@ -1,51 +1,20 @@
-hotel = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-room = [ ]
-taken = 5
-
-print("*********************************")
-print("welcome to hotel costal Rings!")
-print("*********************************")
-print("please do choose room number you would love to stay in ")
-print("###############################")
-print("the are 20 rooms available")
-print("###############################")
-
-while taken >= 1 :
-   choice = int(input("Enter the room number of your choice: ")) 
-
-   
-   if choice not in hotel:
-     print("room number is don't exists")
-   
-   elif choice in room:
-     print("sorry room is taken already")
-     print("please choose another room")
-  
-   else:
-     room.append(choice)
-     print("welcome Room of choice is available")
-     print("Here is your room key") 
-   taken -= 1
-   
-  
-print("\n")
-print("checking in for available rooms")
-
-for i in hotel:  
-    mit = [ ] 
-    if i not in room:
-      mit.append(i)
-      print((mit),end = " ")
-    
-else:
-  print("\n")
-  print("This are the available rooms remaining")
-    
-
-
-
-   
-     
-   
-
-
+#Mad lib game
+print("*************************")
+print("Let's play a fun game")
+print("*************************")
+name = input("whats your name?  \n")
+age = int(input("How old are you? \n "))
+book = input("what's your favourite book? \n ")
+colour = input("What's your favourite colour? \n")
+food = input("what's your favourite food? \n")
+sports = input("what's your favourite sports? \n")
+celebrity = input("who is your favourite celebrity? \n")
+flower = input("what's your favourite flower? \n")
+print(f'''Hi my name is {name},am {age} years old.i love reading alot,my favourite book is {book}!,
+i don't have much hobby but i love reading,and 
+watching YouTube for fun. my favourite colour is {colour} and my favourite food is {food}.
+i also love sports,my favourite sports are {sports},
+My favourite celebrity is {celebrity},am really
+a huge fan,and my favourite flower is {flower}.
+one day i want to meet {celebrity}, it would be the 
+happiest day for me.''')
