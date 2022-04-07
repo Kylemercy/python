f32928a342d8ed5ee0b17be8eb22716d266766cc
root <root@localhost>
2022-04-07 13:47:10 +0100

    auto publish 1649335630458

--- src/main.py
+++ src/main.py
@@ -1,33 +1,16 @@
-
-print("________________________________")
-print("________________________________")
-print("Temperature conversion system")
-print("________________________________")
-print("________________________________")
-print("selections: \n press 1 To enter Celsius \n press2 To enter Fahrenheit \n press 3 To enter Kelvin ")
-select = int(input("Enter your selection: "))
-if select  > 3:
-  print("invalid input")
-if select == 1:
-  temp = float(input("Enter your temperature in Celsius: "))
-  fahrenheit = round((((9/5)*temp)+32),2)
-  Kelvin = round((temp + 273.15),2)
-  print("\n")
-  print("Temperature in Fahrenheit is:", fahrenheit,"fahrenheit,deg")
-  print("Temperature in Kelvin is :", Kelvin,"Kelvin,deg")
-
-elif select  == 2:
-  temp = float(input("Enter your selection in Fahrenheit: "))
-  celsius = round(((5/9)*(temp -32)),2)
-  Kelvin = round(((temp-459.67)*(5/9)),2)
-  print("\n")
-  print("Temperature in Celsius is : ", celsius,"Celsius,deg")
-  print("Temperature in Kelvin is :", Kelvin,"Kelvin,deg")
- 
-
-elif select == 3:
-  temp = float(input("Enter temperature in Kelvin: "))
-  fahrenheit = round(((temp*(9/5))-459.67),2)
-  celsius = round((temp-273.15), 2)
-  print('Temperature in Farenheit: ', fahrenheit, 'deg F')
-  print('Temperature in Celcius: ', celsius, 'deg C')
+import random
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower = "abcdefghijklmnopqrstuvwxyz"
+number = "0123456789"
+symbol =" @#*.,&!?"
+all = upper+lower+symbol+number
+length = 15
+password = "".join(random.sample(all, length))
+print("The password generated for you is ", password)
+name = input("what's your name? \n")
+s_name = input("what's your surname? \n")
+gmail = "@gmail.com"
+mail =  number + symbol + gmail
+len = 7
+g_mail = "".join(random.sample(mail,len))
+print(f"The email generated for you is {name}{s_name}{g_mail}{gmail}")
