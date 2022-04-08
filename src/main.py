2201d0b3b363470e01d0982fef6c7c72221fc796
root <root@localhost>
2022-04-08 17:50:23 +0100

    auto publish 1649436623089

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
-def get_second_largest(user_num):
-  largest = user_num[0]
-  second_largest = user_num[0]
-  for i in range(1,len(user_num)):
-    if user_num[i] > largest:
-      second_largest = largest
-      largest = user_num[i]
-    elif user_num[i] > second_largest:
-      second_largest = user_num[i]
-  return second_largest
 
-user_num = [44,11,83,29,25,76,97,88] 
-results = get_second_largest(user_num)
-print(f"The second largest number is: {results}")
-    
+# To convert from base 10 to base 2
+def dec_to_binary(num):
+  bits = [ ]
+  while num > 0:
+    bits.append(num % 2)
+    num = num // 2
+  bits.reverse()
  
+  binary = " "
+  for bit in bits:
+     binary += str(bit)
+  return binary
+  another = int(input("Enter your number in base10: "))
+ 
+
+  
+num = int(input("Enter your number in base10: "))   
+result = dec_to_binary(num)
+print("you number in base2 is:", result)
+    
+
