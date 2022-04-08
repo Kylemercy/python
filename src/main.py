cfa1c85fc6d00662916f2ee1b074ae5b8a4ad29d
root <root@localhost>
2022-04-08 14:46:23 +0100

    auto publish 1649425583762

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
-num = input("Enter a big number :")
+def get_second_largest(user_num):
+  largest = user_num[0]
+  second_largest = user_num[0]
+  for i in range(1,len(user_num)):
+    if user_num[i] > largest:
+      second_largest = largest
+      largest = user_num[i]
+    elif user_num[i] > second_largest:
+      second_largest = user_num[i]
+  return second_largest
+
+user_num = [44,11,83,29,25,76,97,88] 
+results = get_second_largest(user_num)
+print(f"The second largest number is: {results}")
+    
  
-sum = 0
-for char in num :
-   sum = sum + int(char)
- 
-print(sum)
