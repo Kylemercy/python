4d548b583771f178bad9556727c66b1d33a562b3
root <root@localhost>
2022-04-08 14:07:30 +0100

    auto publish 1649423250624

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
-# email slicer
-email = input("Enter your email: \n").strip()
-username = email[: email.index("@")]
-domain = email[email.index("@")+1:]
-print(f"your username is {username}.\ndomain name is {domain}.")
+def divisible_by_3and5(num):
+  result = [ ]
+  for i in range(num):
+    if i % 3 and i % 5 == 0:
+      result.append(i)
+  return result
+
+
+num = int(input("Enter a number: "))
+total = divisible_by_3and5(num)
+print(f"numbers divisible by 3 and 5 in range {num} are:\n", total)
+    
+  
+
+
+
+
+
+
