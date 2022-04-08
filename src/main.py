9333cd2d1e8ff582a66322fdb0d1cef64b829637
root <root@localhost>
2022-04-08 11:13:30 +0100

    auto publish 1649412810234

--- src/main.py
+++ src/main.py
@@ -1,41 +1,43 @@
 import random
-print("xxxxxxxxxxxxxxxxxxx")
-print("Guessing Game!")
-print("xxxxxxxxxxxxxxxxxxx")
-print("Guess the secret number: it has two digits")
-secret_num = str(random.randint(10,99))
-remaining_try = 7
-while remaining_try > 0:
-  guess = (input("Guess the secret number: "))
-
-  if secret_num == guess:
-    print("Congratulations guessed the number correctly!")
-    print("You Win\nGame over")
+while True:
+  user_action = input("Enter a choice:\nrock\npaper\nscissors.\n")
+  possible_action = ["rock","paper","scissors"]
+  comp_action = random.choice(possible_action)
+  print(f"you choose {user_action} and computer choose {comp_action}")
+  
+  if user_action == comp_action:
+    print(f"Both players selected {user_action}. it's a tie")
+  
+  elif user_action == "paper":
+    if comp_action == "rock":
+      print("you win.\n paper cover's rock")
+    else:
+      print("you lose.\n scissors cuts paper")
+  
+  elif user_action == "rock":
+    if comp_action == "scissors":
+      print("you win!\n rock smaches scissors.")
+    
+    else:
+      print("you lose!\npaper covers rock")
+ 
+  elif user_action == "scissors":
+    if comp_action == "paper":
+      print("you win!\n scissors cuts paper")
+    
+    else:
+      print("you lose!\n rock smaches scissors")
+  
+  play_again = input("To play again enter: yes or no\n")
+  if play_again.lower() != "yes":
     break
-  else:
-    bull = 0
-    cow = 0
-    if secret_num[0] == guess[0]:
-      bull+=1
-    
-    if secret_num[1] == guess[1]:
-      bull += 1
-    
-    if secret_num[1] == guess[0]:
-      cow += 1
-    
-    if secret_num[0] == guess[1]:
-      cow += 1
-    print("Bulls ",bull)
-    print("Cows",cow)
-    remaining_try -= 1
-    if remaining_try <= 1:
-       print("You lost the Game")
-       print("the secret number is ",secret_num)
-       break
-     
+  
     
     
-        
+  
+    
+    
+  
+    
     
   
