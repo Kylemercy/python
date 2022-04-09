a1b0ba82809d631be96086adfd10d2e98704c19d
root <root@localhost>
2022-04-09 18:04:27 +0100

    auto publish 1649523867670

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import datetime
 tday = datetime.date.today()
 tdelta = datetime.timedelta(days = 7)
-print(tdelta + tday)
-print(tday-tdelta )
+bday = datetime.date(2022,5,14)
+till_bday = (bday-tday)
+print(till_bday)
+print(till_bday.days)
+print(till_bday.total_seconds())
