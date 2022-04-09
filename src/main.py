d03c626655e452fc8653893b98a02bc161157e68
root <root@localhost>
2022-04-09 17:56:35 +0100

    auto publish 1649523395198

--- src/main.py
+++ src/main.py
@@ -1,10 +1,4 @@
-from datetime import datetime
- 
-# Getting current date and time
-
-now = datetime.now()
- 
-
-string = datetime.timetuple(now)
-#Returns an object of type time.struct_time
-print (string)
+import datetime
+tday = datetime.date.today()
+tdelta = datetime.timedelta(days = 7)
+print(tdelta + tday)
