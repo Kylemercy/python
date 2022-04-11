882b69c2dd055f1003a7aa07a2b4c87406394260
root <root@localhost>
2022-04-11 15:16:28 +0100

    auto publish 1649686588362

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from datetime import datetime
-t = datetime.now()
-m = datetime.time(t)
-print(m)
-print (m.hour)
-print (m.minute)
-print (m.second)
+import datetime
+b_day = datetime.date(year= 2000,month = 3,day = 24)
+b_day2 = datetime.date(year = 1988,month= 5,day = 27)
+
+if b_day < b_day2:
+  print("Person one is older")
+elif b_day > b_day2:
+  print("Person two is older")
+else:
+  print("The ae both the same age")
+
