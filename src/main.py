a2cbeb24e523eec02f6d85b67916dd78a9eb6bad
root <root@localhost>
2022-04-11 16:49:37 +0100

    auto publish 1649692177296

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from datetime import datetime 
-Christmas = datetime(month = 12,day = 25,year = 1)
-today = datetime.today()
-Christmas= Christmas.replace(year = datetime.today().year)
-count = Christmas- today
-print("There are ",count.days,"remaining till Christmas!")
-
-
+import datetime
+import time 
+print( f"Today date and time is: \n{datetime.datetime.today()}")
+print ("The current year is",datetime.date.today().strftime("%Y"))
+print("The current month is:",datetime.date.today().strftime("%B"))
+print("week number of the year is:",datetime.date.today().strftime("%W"))
+print("weekday of the week is: ",datetime.date.today().strftime("%w"))
+print("Day of the year is :", datetime.date.today().strftime("%j")," days")
+print("Day in of the month is;",datetime.date.today().strftime("%D"))
+print("Day in of the month is;",datetime.date.today().strftime("%d"))
+print("Day in the week",datetime.date.today().strftime("%A"))
