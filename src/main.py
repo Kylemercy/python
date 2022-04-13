3c44f2edce36d55d03c48e467a225cc68021a527
root <root@localhost>
2022-04-13 13:17:28 +0100

    auto publish 1649852248577

--- src/main.py
+++ src/main.py
@@ -1,11 +1,5 @@
-import datetime
-import time 
-print( f"Today date and time is: \n{datetime.datetime.today()}")
-print ("The current year is",datetime.date.today().strftime("%Y"))
-print("The current month is:",datetime.date.today().strftime("%B"))
-print("week number of the year is:",datetime.date.today().strftime("%W"))
-print("weekday of the week is: ",datetime.date.today().strftime("%w"))
-print("Day of the year is :", datetime.date.today().strftime("%j")," days")
-print("Day in of the month is;",datetime.date.today().strftime("%D"))
-print("Day in of the month is;",datetime.date.today().strftime("%d"))
-print("Day in the week",datetime.date.today().strftime("%A"))
+from datetime import datetime
+tday = datetime.now()
+m_day = datetime.time(tday)
+print(m_day,m_day.strftime("%p"))
+#p help it shows either pm or am
