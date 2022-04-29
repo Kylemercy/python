2bd4f1fd73ebb47e27427c2aad378fcf72e69503
root <root@localhost>
2022-04-29 10:44:00 +0100

    auto publish 1651225440419

--- src/main.py
+++ src/main.py
@@ -1,35 +1,14 @@
-class Grocery:
-  def __init__(self,name, quantity,price):
-    self.name = name
-    self.quantity = quantity
-    self.price = price
+class Car:
+  def __init__(self, colour,miles):
+    self.colour = colour
+    self.miles = miles
   
-  def total(self):
-    return self.price*self.quantity
-
-
-item1 = Grocery("phone",50,7000)
-item2= Grocery("laptop",89,9600)
-print(item2.total())
-print(item1.total())
-
-
-#the first method or second
-#the first is better
-class Grocery:
-item1.phone = "phone"
-item1.quantity = 70
-item1.price = 700
-
-item2 = Grocery()
-item2.phone = "laptop"
-item2.quantity = 60
-item2.price = 800
-
-
-print(item2.calculate(item2.quantity,item2.price))
-  
+  def description(self):
+    return f"The {self.colour} car has {self.miles:,}."
   
 
-  
 
+car1 = Car("Red",60_000)
+car2 = Car("black",89_000)  
+print(car1.description())
+print(car2.description())
