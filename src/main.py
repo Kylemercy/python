2bd7a6f3e3547dde38e08354543d8be1ff8382e4
root <root@localhost>
2022-05-02 06:57:02 +0100

    auto publish 1651471022075

--- src/main.py
+++ src/main.py
@@ -1,70 +1,21 @@
-
-
-class Article:
-    def __init__(self, title, page_count):
-        # initialize protected attributes
-        self._title = title
-        self._page_count = page_count
-         
-    # define protected method
-    def _show(self):
-        # access protected attributes inside class 
-        print("Article Title: ", self._title)
-        print("Page Count: ", self._page_count)
-         
-class Author(Article):
-    def __init__(self, name, title, page_count):
-        Article.__init__(self, title, page_count)
-        self.name = name
-    def display(self):
-        print("Author Name: ", self.name)
-        # access Article's protected method
-        self._show()
-        print("------------------ \n")
-         
-         
-author = Author("Eyong Kevin", "Python Classes and Objects", 3000)
-author.display()
-# access protected data
-print(author._title) 
-
-
-
-
-
-
-
-class Article:
-  def __init__(self, title,pages):
-    self._title = title
-    self._pages = pages
-  
-  
-  def _show(self):
-    #_is use to make a protected method
-    print("The Article title is ",self._title,".")
-    print("The number of pages is ",self._pages,".")
-    
-    
-class Author(Article):
-  def __init__(self,name,title,pages):
-    Article.__init__(self, title,pages)
-    self.name = name
-  
-  
-  def display(self):
-    print("Author name: ",self.name)
-    self._show()
-    print("----------------------\n")
-
-author = Author("adele","songs of echo",46)
-author.display()
-print(author._title)
-  
-  
-
-  
-
-
-
-
+class Cylinder:
+    # class attribute
+    pi = 3.14
+    def __init__(self, radius, height):
+        # instance variables
+        self.radius = radius
+        self.height = height
+ 
+    # instance method
+    def volume(self):
+        return Cylinder.pi * self.radius**2 * self.height
+ 
+    # class method
+    @classmethod
+    def description(cls):
+        return f'This is a Cylinder class that computes the volume using Pi={cls.pi}'
+c1 = Cylinder(4, 2) # create an instance/object
+print(f'Volume of Cylinder: {c1.volume()}') # access instance method 
+print(Cylinder.description()) # access class method via class
+print(c1.description()) # access class method via instance
+print(type(c1))
