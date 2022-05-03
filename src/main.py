291678d2f561a4760e532705c5f220cd12406914
root <root@localhost>
2022-05-03 23:38:51 +0100

    auto publish 1651617531735

--- src/main.py
+++ src/main.py
@@ -1,42 +1,25 @@
-class Employee:
-    # constructor
-    def __init__(self, name, salary):
-        # public data member
+# base class
+class Company:
+    def __init__(self):
+ # Protected member having a single underscore
+        self._project = "NLP"
+        
+
+# child class
+class Employee(Company):
+    def __init__(self, name):
         self.name = name
-        # private member
-        self.__salary = salary
-#__that is double underscore use to create a private object
-     
+        #super().__init__()
+        Company.__init__(self)
+      #this use to inherit the project attribute
+
     def show(self):
-        # private members are accessible from a class
-        print("Name: ", self.name, 'Salary:', self.__salary)
+        print("Employee name :", self.name)
+   # Accessing protected member in child class
+        print("Working on project :", self._project)
 
-# creating object of a class
-# creating object of a class
-emp = Employee('Jessa', 10000)
+c = Employee("Jessa")
+c.show()
 
-# accessing private data members
-#to Access the private attribute we do this
-#we create a method for the attributes as seen
-#we then call the method
-emp.show() 
-#or my using the mangling method which is
-# there is no need to create a method
-#the private variable can be called directly 
-
-
-
-
-class Employee:
-    # constructor
-    def __init__(self, name, salary):
-        # public data member
-        self.name = name
-        # private member
-        self.__salary = salary
-
-eyu = Employee('messa', 58000)
-#to Access the private variable salary
-print (eyu._Employee__salary)
-#this is called the magling method of accessing
-#private variable 
+# Direct access protected data member
+print('Project:', c._project)
