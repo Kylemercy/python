69bf002d9c65a8a2e212ca1d38bca683e382de90
root <root@localhost>
2022-05-03 23:14:40 +0100

    auto publish 1651616080503

--- src/main.py
+++ src/main.py
@@ -20,3 +20,23 @@ emp = Employee('Jessa', 10000)
 #we create a method for the attributes as seen
 #we then call the method
 emp.show() 
+#or my using the mangling method which is
+# there is no need to create a method
+#the private variable can be called directly 
+
+
+
+
+class Employee:
+    # constructor
+    def __init__(self, name, salary):
+        # public data member
+        self.name = name
+        # private member
+        self.__salary = salary
+
+eyu = Employee('messa', 58000)
+#to Access the private variable salary
+print (eyu._Employee__salary)
+#this is called the magling method of accessing
+#private variable 
