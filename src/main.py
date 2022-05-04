68bcc1bc177d21f2541af46877ab966e87ab6038
root <root@localhost>
2022-05-04 22:55:06 +0100

    auto publish 1651701306372

--- src/main.py
+++ src/main.py
@@ -1,20 +1,100 @@
-#polymorphism
-# Python program to demonstrate in-built poly-
-# morphic functions
+#polymorphism in classes
+class India:
+    def capital(self):
+        print("New Delhi is the capital of India.")
+    def language(self):
+
+        print("Hindi is the most widely spoken language of India.")
  
-# len() being used for a string
 
-print(len("geeks"))
+    def type(self):
+
+        print("India is a developing country.")
  
-# len() being used for a list
 
-print(len([10, 20, 30])
-# A simple Python function to demonstrate 
-# Polymorphism
-def num(x,y,z = 0):
-  return x + y + z
+class USA:
 
-print(num(3,4))
-print(num(2,5,6))
+    def capital(self):
+
+        print("Washington, D.C. is the capital of USA.")
+ 
+
+    def language(self):
+
+        print("English is the primary language of USA.")
+ 
+
+    def type(self):
+
+        print("USA is a developed country.")
+ 
+
+obj_ind = India()
+
+obj_usa = USA()
+for country in(obj_ind,obj_usa):
+  country.capital()
+  country.language()
+  country.type()
+  
+  
+#polymorphism with a function
+class India():
+
+    def capital(self):
+
+        print("New Delhi is the capital of India.")
 
   
+
+    def language(self):
+
+        print("Hindi is the most widely spoken language of India.")
+
+  
+
+    def type(self):
+
+        print("India is a developing country.")
+
+  
+
+class USA():
+
+    def capital(self):
+
+        print("Washington, D.C. is the capital of USA.")
+
+  
+
+    def language(self):
+
+        print("English is the primary language of USA.")
+
+  
+
+    def type(self):
+
+        print("USA is a developed country.")
+ 
+
+def func(obj):
+
+    obj.capital()
+
+    obj.language()
+
+    obj.type()
+
+  
+
+obj_ind = India()
+
+obj_usa = USA()
+
+  
+func(obj_ind)
+func(obj_usa)
+
+  
+
