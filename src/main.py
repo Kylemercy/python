d8e04ef8dc6568817680ff551e8ff0d245b947f4
root <root@localhost>
2022-05-05 13:28:33 +0100

    auto publish 1651753713659

--- src/main.py
+++ src/main.py
@@ -1,32 +1,13 @@
-# Use super()
-class Doughnut:
-    def __init__(self, flavor, r_in, r_out, topping):
-        self.insideRadius   = r_in
-        self.outsideRadius  = r_out
-        self.topping = topping
-        self.flavor = flavor
-        
-       #  is wrong. YOu do not need 'self'
+class Company:
+    def company_name(self):
+        return 'Google'
 
-    def donutarea(self):
-        return 3.1416 * (self.outsideRadius ** 2 - self.insideRadius ** 2)
-    
-    def donutperimeter(self):
-        return 2 * 3.1416 * (self.insideRadius +  self.outsideRadius)
-    
-    def inarea(self):
-      return 2* 3.1416*self.insideRadius ** 2
-    
-      
-    
-class glazedDonut(Doughnut):
-  def __init__(self,flavor,r_in,r_out,topping):
-   super().__init__(flavor, r_in, r_out, topping)
-                          
-emp = glazedDonut("chocolate",4,6,"glazed")
-print("We just made a " + emp.topping + ' ' + emp.flavor + " donut.")
-print("It's area is: ",emp.donutarea())
-print("And it's perimeter is: ")
-print(emp.donutperimeter())
-print("And the inside hollow area is: ")
-print(emp.inarea())
+class Employee(Company):
+    def info(self):
+        # Calling the superclass method using super()function
+        c_name = super().company_name()
+        print("Jessa works at", c_name)
+
+# Creating object of child class
+emp = Employee()
+emp.info()
