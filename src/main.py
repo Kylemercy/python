fedd8508840a297e3a3d1e082dbf25cd2fd952bc
root <root@localhost>
2022-05-05 16:45:19 +0100

    auto publish 1651765519270

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 class Student:
-    def __init__(self, roll_no, name):
+    # constructor
+    def __init__(self, name, age):
         # Instance variable
-        self.roll_no = roll_no
         self.name = name
+        self.age = age
 
-s1 = Student(10, 'Jessa')
-print('Instance variable object has')
-print(s1.__dict__)
-
-# Get each instance variable
-for key_value in s1.__dict__.items():
-    print(key_value[0], '=', key_value[1])
+    # instance method to access instance variable
+    def show(self):
+        print('Name:', self.name,"\n" 'Age:', self.age)
+c1 = Student("mike",45)
+c1.show()
