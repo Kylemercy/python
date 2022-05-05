ab91b2d102c637859dbf16aa6f0d9fb87ee0b7db
root <root@localhost>
2022-05-05 10:16:22 +0100

    auto publish 1651742182540

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
-print('python is awesome'.replace('python', 'freeCodeCamp'))
-print('python is awesome'.split(","))
-print(' '.join(['python',' is','awesome.']))
-#join i use to join a valuae in a list
+#A SIMPLE CALCULATOR
+a = float(input('First: '))
+b = float(input('Second: '))
+op = input('Operation (sum/sub/mul/div): ')
+
+if op == 'sum':
+    print(f'a + b = {a+b}')
+elif op == 'sub':
+    print(f'a - b = {a-b}')
+elif op == 'mul':
+    print(f'a * b = {a*b}')
+elif op == 'div':
+    print(f'a / b = {a/b}')
+else:
+    print('Invalid Operation!')
