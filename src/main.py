8cca6ad94646ce20de7891dbdc919bd09c306876
root <root@localhost>
2022-05-06 11:00:48 +0100

    auto publish 1651831248522

--- src/main.py
+++ src/main.py
@@ -1,18 +1,14 @@
 class Student:
     # Class variable
-#shared by all objects created s1,s2
     school_name = 'ABC School '
-    #class variable
-    def __init__(self, name, roll_no):
+
+    # constructor
+    def __init__(self, name):
         self.name = name
-        self.roll_no = roll_no
+        # access class variable inside constructor using self
+        print(self.school_name)
+        # access using class name
+        print(Student.school_name)
 
-# create first object
-s1 = Student('Emma', 10)
-print(s1.name, s1.roll_no, Student.school_name)
-# access class variable
-
-# create second object
-s2 = Student('Jessa', 20)
-# access class variable
-print(s2.name, s2.roll_no, Student.school_name)
+# create Object
+s1 = Student('Emma')
