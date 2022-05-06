76c0a75c49fa526a969f746a8d467a11677f67ec
root <root@localhost>
2022-05-06 11:23:49 +0100

    auto publish 1651832629208

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
-#it has both a class and instance variables
-class Car:
-    # Class variable
-    manufacturer = 'BMW'
+#DIFFERENT WAYS OF CREATING A CLASS VARIABLE
+#WITH DIFFERENT CHILD CLASS
 
-    def __init__(self, model, price):
-        # instance variable
-        self.model = model
-        self.price = price
+class Course:
+    # class variable
+    course = "Python"
 
-# create Object
-car = Car('x1', 2500)
-print(car.model, car.price, Car.manufacturer)
+class Student(Course):
+
+    def __init__(self, name):
+        self.name = name
+
+    def show_student(self):
+        # Accessing class variable of parent class
+        print('Before')
+        print("Student name:", self.name, "Course Name:", Student.course)
+ # changing class variable value of base class
+        print('Now')
+        Student.course = "Machine Learning"
+        print("Student name:", self.name, "Course Name:", Student.course)
+
+# creating object of Student class
+stud = Student("Emma")
+stud.show_student()
