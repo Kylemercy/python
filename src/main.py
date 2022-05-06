a2952a5cb24461a3baca74b41d94e8c0a039afde
root <root@localhost>
2022-05-06 11:07:33 +0100

    auto publish 1651831653864

--- src/main.py
+++ src/main.py
@@ -11,4 +11,35 @@ class Student:
         print(Student.school_name)
 
 # create Object
-s1 = Student('Emma')
+s1 = Student("mike")
+print(s1.name)
+
+
+class Student:
+    # Class variable
+    school_name = 'ABC School '
+
+    # constructor
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+
+    # Instance method
+    def show(self):
+        print('Inside instance method')
+        # access using self
+        print(self.name, self.roll_no, self.school_name)
+        # access using class name
+        print(Student.school_name)
+
+# create Object
+s1 = Student('Emma', 10)
+s1.show()
+
+print('Outside class')
+# access class variable outside class
+# access using object reference
+print(s1.school_name)
+
+# access using class name
+print(Student.school_name)
