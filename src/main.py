b420264268528690c3a7351e8362e5ebfc6a4fb6
root <root@localhost>
2022-05-07 00:33:58 +0100

    auto publish 1651880038768

--- src/main.py
+++ src/main.py
@@ -1,36 +1,15 @@
-class Student:
-    # class variables
-    school_name = 'ABC School'
+class Vehicle:
+  def __init__(self, distance, mileage):
+    self.distance = distance
+    self.mileage = mileage
+  
+  
+  def max_speed(self):
+    print(self.distance/self.mileage)
+   
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    # instance method
-    def show(self):
-        # access instance variables
-        print('Student:', self.name, self.age)
-        # access class variables
-        print('School:', self.school_name)
-
-    @classmethod
-    def change_School(cls, name):
-        # access class variable
-        print('Previous School name:', cls.school_name)
-        cls.school_name = name
-        print('School name changed to', cls.school_name)
-
-    @staticmethod
-    def find_notes(subject_name):
-        # can't access instance or class attributes
-        return ['chapter 1', 'chapter 2', 'chapter 3']
-
-# create object
-jessa = Student('Jessa', 12)
-# call instance method
-jessa.show()
-
-# call class method
-Student.change_School('XYZ School')
-jessa.show()
-#jessa school changes to (xyz school)
+ept = Vehicle(500,40)
+ept.max_speed()
+print(ept.distance)
+print(ept.mileage)
+  
