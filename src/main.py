8072df448b79cf06c2d5616cc2f5b88b47d81540
root <root@localhost>
2022-05-06 11:19:33 +0100

    auto publish 1651832373680

--- src/main.py
+++ src/main.py
@@ -1,31 +1,13 @@
-#It is best practice to use a class name
-# to change the value of a class variable. 
-#Because if we try to change the class 
-#variable’s value by using an object, 
-#a new instance variable is created for that
- #particular object, which shadows the class
-# variables.
-
-
-class Student:
+#it has both a class and instance variables
+class Car:
     # Class variable
-    school_name = 'ABC School '
+    manufacturer = 'BMW'
 
-    # constructor
-    def __init__(self, name, roll_no):
-        self.name = name
-        self.roll_no = roll_no
+    def __init__(self, model, price):
+        # instance variable
+        self.model = model
+        self.price = price
 
-# create Objects
-s1 = Student('Emma', 10)
-s2 = Student('Jessa', 20)
-
-print('Before')
-print(s1.name, s1.roll_no, s1.school_name)
-print(s2.name, s2.roll_no, s2.school_name)
-
-# Modify class variable using object reference
-s1.school_name = 'PQR School'
-print('After')
-print(s1.name, s1.roll_no, s1.school_name)
-print(s2.name, s2.roll_no, s2.school_name)
+# create Object
+car = Car('x1', 2500)
+print(car.model, car.price, Car.manufacturer)
