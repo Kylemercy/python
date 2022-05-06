617d3128d66f152e943a53c7799174dc5d467886
root <root@localhost>
2022-05-07 00:02:12 +0100

    auto publish 1651878131947

--- src/main.py
+++ src/main.py
@@ -1,25 +1,42 @@
-#static method
-#it can be defined using
-#the @staticmethod decorator or 
-#staticmethod() function.
+class Student:
+    # class variables
+    school_name = 'ABC School'
 
+    # constructor
+    def __init__(self, name, age):
+        # instance variables
+        self.name = name
+        self.age = age
 
-#using static method function
-class Test:
-  @staticmethod
-  def static1():
-    print("static method 1")
-  
-  def static2():
-    Test.static1()
-  
-  
-  @classmethod
-  def class_method(cls):
-    cls.static2()
+    # instance variables
+    def show(self):
+        print(self.name, self.age, Student.school_name)
 
-test = Test()
-test.class_method()
-  
-  
-  
+    @classmethod
+    def change_School(cls, name):
+        cls.school_name = name
+
+    @staticmethod
+    def find_notes(subject_name):
+        return ['chapter 1', 'chapter 2', 'chapter 3']
+        
+        
+# create object
+jessa = Student('Jessa', 12)
+# call instance method
+jessa.show()
+
+# call class method using the class
+Student.change_School('XYZ School')
+jessa.show()
+# call class method using the object
+jessa.change_School('PQR School')
+jessa.show()
+# call static method using the class
+Student.find_notes('Math')
+# call class method using the object
+jessa.find_notes('Math')
+#A static method doesn’t have access 
+#to the class attribute and instance attributes.
+# Therefore, it cannot modify the class or object
+# state.therefore nothing is modified 
