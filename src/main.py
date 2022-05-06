4146d66c0f0edaf3a50d3b69de15d246978eeb09
root <root@localhost>
2022-05-06 10:58:16 +0100

    auto publish 1651831096128

--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
 class Student:
-    # constructor
-    def __init__(self, name, age):
-        # Instance variable
+    # Class variable
+#shared by all objects created s1,s2
+    school_name = 'ABC School '
+    #class variable
+    def __init__(self, name, roll_no):
         self.name = name
-        self.age = age
+        self.roll_no = roll_no
 
-    # instance method to access instance variable
-    def show(self):
-        print('Name:', self.name,"\n" 'Age:', self.age)
-c1 = Student("mike",45)
-c1.show()
+# create first object
+s1 = Student('Emma', 10)
+print(s1.name, s1.roll_no, Student.school_name)
+# access class variable
+
+# create second object
+s2 = Student('Jessa', 20)
+# access class variable
+print(s2.name, s2.roll_no, Student.school_name)
