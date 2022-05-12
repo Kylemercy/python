9bacedaab8c3b79778bf77cda876f50c2b585354
root <root@localhost>
2022-05-12 10:24:44 +0100

    auto publish 1652347484027

--- src/main.py
+++ src/main.py
@@ -1,19 +1,10 @@
-class Car:
-    car_name = "Wheeljack"
-    def move(self):
-        print(self.car_name, "Car is Moving")
+def main():
+  print(f"first module name is : {__name__}")
+  
+  
+if __name__ == '__main__':
+  main()
 
-class Truck:
-    truck_name = "Optimus Prime"
-    def move(self):
-        print( self.truck_name, "Truck is moving")
+#this code helps to prevent your code from
 
-class Bike:
-    bike_name = "Elita-One"
-    def move(self):
-        print(self.bike_name, "Bike is moving")
-
-vehicles = [Car(), Truck(), Bike()]
-
-for vehicle in vehicles:
-    vehicle.move()
+#running when imported to a different file 
