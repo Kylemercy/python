d409575d739ae1b0f1c928d95f7cb2e77158fc80
root <root@localhost>
2022-05-12 11:30:13 +0100

    auto publish 1652351413058

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
-#decorative function
+#DECORATORS IN PYTHON
 def decoration_fun(original):
-  def wrapped_fun():
+  def wrapper_fun():
+    print(f"our wrapper function excuted this code before {original.__name__}.")
     return original()
-  return wrapped_fun
+  return wrapper_fun
 
+@decoration_fun
 def display():
-  print("decorated original function")
-
+  print("decorated original function.")
+# a shorter format for the above code
+#using a decorator @
+# now we just have to call the display method
+display()
+#the both print thesame code
+print("\n")
 decorative_display = decoration_fun(display)
 decorative_display()
+
+
+
 
   
