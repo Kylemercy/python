d6ec153cdad32fa04e1faa7ecc9e68a2048f78d9
root <root@localhost>
2022-05-13 20:43:39 +0100

    auto publish 1652471019795

--- src/main.py
+++ src/main.py
@@ -1,24 +1,6 @@
-#DECORATORS IN PYTHON
-#decorators using class
-class decoration_class(object):
-  def __init__(self, original):
-    self.original = original
+words = ["drum","cat","fish"]
+counts = [1,4,6]
+for word,count in zip(words, counts):
+  print(word,":",count)
+
   
-  def __call__(self,*args,**kwargs):
-    print(f"our call method excuted this code before {self.original.__name__}.")
-    return self.original(*args,**kwargs)
-
-@decoration_class
-def display():
-  print("decorated original function.")
-
-@decoration_class
-def display_info(name,age):
-  print(f"display function ran with argument {name}, and {age}")
-
-display()
-print("\n")
-display_info("sid",34)
-  
-
-
