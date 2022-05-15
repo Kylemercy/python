8a2d412da94d4dd1c9db1bb501da7ba92417b4e2
root <root@localhost>
2022-05-15 01:59:43 +0100

    auto publish 1652576383362

--- src/main.py
+++ src/main.py
@@ -1,23 +1,14 @@
 import time
-def before_decorated(func):
+def timmer(func):
   def wrapper(*args):
-    print("before")
+    before = time.time()
     func(*args)
-    print("after")
-  
+    print("function took",time.time() - before,"seconds")
   return wrapper
+    
+@timmer  
+def run():
+  time.sleep(2)
+#note this function will run ater 2 seconds
 
-
-class Time():
-  @before_decorated
-  # for class the decorator is called inside
-  #a class
-
-  def decorated_method(self):
-    print(time.ctime())
-
-t = Time()
-t.decorated_method()
-  
-
-  
+run()
