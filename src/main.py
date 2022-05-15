1b04a96aaddc1ee0917c69ece63791a07a810eb6
root <root@localhost>
2022-05-15 01:50:30 +0100

    auto publish 1652575830003

--- src/main.py
+++ src/main.py
@@ -1,26 +1,23 @@
-#returning value from a decorated function
-def f1(func):
-  def wrapper(*args,**kwargs):
-    print("started")
-    val = func(*args,**kwargs)
-    # asign the function to a variable
-    #the function that is being wrapped
-    print("ended")
-    return val
-    
-    # the return that variable
+import time
+def before_decorated(func):
+  def wrapper(*args):
+    print("before")
+    func(*args)
+    print("after")
+  
   return wrapper
 
-@f1
-def f(a,b= 30):
-  print(f"{a} is {b} years old.")
 
-f("ada")
+class Time():
+  @before_decorated
+  # for class the decorator is called inside
+  #a class
 
+  def decorated_method(self):
+    print(time.ctime())
 
-@f1
-def add(a,b):
-  return a + b
-print(add(4,5))
+t = Time()
+t.decorated_method()
+  
 
-
+  
