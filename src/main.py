a04f284d2527ecb3c1e48563643dee220faef105
root <root@localhost>
2022-05-21 13:56:13 +0100

    auto publish 1653137773279

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
-#fist class function
-#and closures
-def html_tag(tag):
+#CLOSURES
+def outer_fun():
+  msg = "Hello"
+  def inner_fun():
+    print(msg)
   
-  def wrap(txt):
-    print(f' <{tag}> {txt} \<{tag}> ')
-  
-  return wrap
+  return inner_fun
+#this won't exceute as the inner func wasn't
+#called using()
+outer_fun()
+#to execute this we assign it to a variable
 
-html_p = html_tag("Hi")
-html_p("Nice meeting you!")
+my_fun = outer_fun()
+my_fun()
+# this was executed
+
+#another way is by calling the inner func
   
-html_t = html_tag("Bye")
-html_t("Have a nice day")
+def outer_fun():
+  msg = "Hello"
+  def inner_fun():
+    print(msg)
+  
+  return inner_fun()
+  #the inner func was called using ()
+outer_fun()
+# this is now executed
+  
