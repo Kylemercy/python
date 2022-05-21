170048490fd18d5016c5f2f76310b5c0611f0e82
root <root@localhost>
2022-05-21 11:25:47 +0100

    auto publish 1653128747250

--- src/main.py
+++ src/main.py
@@ -1,5 +1,31 @@
-sports = {'Ronaldo' : 'Soccer', 'Lebron': 'Basketball', 'Federer' : 'Tennis'}
-for key in sports.keys():
-  value = sports[key]
-  print('Sports:', value)
- #to get the value in the dictionary 
+# importing libraries
+import time
+import math
+# decorator to calculate duration
+# taken by any function.
+def calculate_time(func):
+    # added arguments inside the inner1,
+    # if function takes any arguments,
+    # can be added like this.
+
+    def inner1(*args, **kwargs):
+        # storing time before function execution
+        begin = time.time()
+        func(*args, **kwargs)
+        # storing time after function execution
+        end = time.time()
+        print("Total time taken in :  ", func.__name__, end - begin)
+
+    return inner1
+# this can be added to any function present,
+# in this case to calculate a factorial
+@calculate_time
+def factorial(num):
+    # sleep 2 seconds because it takes very less time
+    # so that you can see the actual difference
+    time.sleep(2)
+    print("factorial of number is  ",math.factorial(10))
+    
+# calling the function.
+
+factorial(10)
