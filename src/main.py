b2eff3f9d034938a0b4a9f7406a1a8330419c32b
root <root@localhost>
2022-05-23 00:07:15 +0100

    auto publish 1653260835330

--- src/main.py
+++ src/main.py
@@ -1,28 +1,22 @@
-#CLOSURES
-def outer_fun():
-  msg = "Hello"
-  def inner_fun():
-    print(msg)
-  
-  return inner_fun
-#this won't exceute as the inner func wasn't
-#called using()
-outer_fun()
-#to execute this we assign it to a variable
-
-my_fun = outer_fun()
-my_fun()
-# this was executed
-
-#another way is by calling the inner func
-  
-def outer_fun():
-  msg = "Hello"
-  def inner_fun():
-    print(msg)
-  
-  return inner_fun()
-  #the inner func was called using ()
-outer_fun()
-# this is now executed
-  
+def repeat_decorator(num_repeats = 2):
+# repeat_decorator should return a 
+#function that's a decorator
+    def inner_decorator(fn):
+        def decorated_fn():
+            for i in range(num_repeats):
+                fn()
+        #fn is function that is being decorated
+        # return the new function
+        return decorated_fn
+# return the decorator that actually takes
+# the function in as the input
+    return inner_decorator
+ 
+# use the decorator with num_repeats argument 
+#set as 5 to repeat the function call 5 times
+@repeat_decorator(5)
+def hello_world():
+    print("Hello world!")
+ 
+# call the function
+hello_world()
