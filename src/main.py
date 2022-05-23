1c0918efc3b787c9564222fba4e4658510042546
root <root@localhost>
2022-05-23 01:09:01 +0100

    auto publish 1653264541669

--- src/main.py
+++ src/main.py
@@ -1,22 +1,11 @@
-def repeat_decorator(num_repeats = 2):
-# repeat_decorator should return a 
-#function that's a decorator
-    def inner_decorator(fn):
-        def decorated_fn():
-            for i in range(num_repeats):
-                fn()
-        #fn is function that is being decorated
-        # return the new function
-        return decorated_fn
-# return the decorator that actually takes
-# the function in as the input
-    return inner_decorator
+
+def plus_one(number):
+     print(f" First: {number + 1}")
+     return number + 1
  
-# use the decorator with num_repeats argument 
-#set as 5 to repeat the function call 5 times
-@repeat_decorator(5)
-def hello_world():
-    print("Hello world!")
+def function_call(function):
+     number_to_add = 5
+     print(f" Second: {function(number_to_add)}")
+     return function(number_to_add)
  
-# call the function
-hello_world()
+function_call(plus_one)
