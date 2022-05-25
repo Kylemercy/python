6ce6f2c0ef22437c5a147277e118a86ab83a5d62
root <root@localhost>
2022-05-25 23:58:13 +0100

    auto publish 1653519492888

--- src/main.py
+++ src/main.py
@@ -1,47 +1,41 @@
 class Employee:
-  raise_amount = 1.05
-  num_emp = 0
-  # class Variable 
+  
   def __init__(self,first,last,pay):
     self.first = first
     self.last = last
     self.pay = pay
-    self.email = first + last + "@company.com"
-    Employee.num_emp += 1
-    #this increase as the new employee object
-#is created  
-  def amount_apply(self):
-    self.pay = int(self.pay *self.raise_amount)
-    return (self.pay)
-  
+    
+ 
+  @property
   def fullname(self):
     return "{} {}". format(self.first,self.last)
+#the property decorator allows us to create a 
+#method which can still be called as an attribute
+#example creating an email method but can still
+#be called as an attribute instead of a method
+  @property
+  def email(self):
+     return "{}{}@email.com". format(self.first,self.last)
+#setter
+  @fullname.setter
+  def fullname(self,name):
+    first,last = name.split(' ')
+#(this will split the name given as a single
+# unit into fist,and last)
+    self.first = first
+    self.last = last
+  @fullname.deleter
+  def fullname(self):
+    print("Deleted!")
+    self.first = None
+    self.last = None
   
-  def __repr__(self):
-    return (f"Employee: {self.first},{self.last},{self.pay}")
-  
-  def __str__(self):
-    return (f"{self.display}-{self.email}")
-  
-  def __add__(self,other):
-    return self.pay + other.pay
- 
-  def __len__(self):
-    return len(self.fullname())
+    
   
   
-#special or dunder method
-#use for info overloading
-#using the repr and str magic method
-#repr is use for debugging by programers
-#str is meant for the reader
-     
-dev1 =Employee("sid","daje",6000)
-dev2 = Employee("dash","qote",7000)
-print(dev1.fullname())
-print(dev1+ dev2)
-print(len(dev2))
-print(dev1.amount_apply())
-#note when you use return you have to print out
-#your methods
-
+dev1 = Employee("han","san",4500)
+dv2 = Employee("sid","fax",5000)
+dev1.fullname = "jake mike"
+del(dev1.fullname)
+print (dev1.fullname)
+print(dev1.email)
