58479efd3a9c5ece610e5404a26e257e88b901f3
root <root@localhost>
2022-05-28 16:34:14 +0100

    auto publish 1653752053833

--- src/main.py
+++ src/main.py
@@ -1,41 +1,18 @@
-class Employee:
-  
-  def __init__(self,first,last,pay):
-    self.first = first
-    self.last = last
-    self.pay = pay
-    
- 
-  @property
-  def fullname(self):
-    return "{} {}". format(self.first,self.last)
-#the property decorator allows us to create a 
-#method which can still be called as an attribute
-#example creating an email method but can still
-#be called as an attribute instead of a method
-  @property
-  def email(self):
-     return "{}{}@email.com". format(self.first,self.last)
-#setter
-  @fullname.setter
-  def fullname(self,name):
-    first,last = name.split(' ')
-#(this will split the name given as a single
-# unit into fist,and last)
-    self.first = first
-    self.last = last
-  @fullname.deleter
-  def fullname(self):
-    print("Deleted!")
-    self.first = None
-    self.last = None
-  
-    
-  
-  
-dev1 = Employee("han","san",4500)
-dv2 = Employee("sid","fax",5000)
-dev1.fullname = "jake mike"
-del(dev1.fullname)
-print (dev1.fullname)
-print(dev1.email)
+def binary_search(my_list,target):
+  low = 0
+  high = len(my_list) - 1
+  while low<= high:
+      mid = (low + high)//2
+      guess = my_list[mid]
+      if guess == target:
+        return mid
+      if mid >= target:
+        high = mid - 1
+      
+      else:
+        low =  mid +1
+  return None
+      
+my_list = [1,2,6,7,8,8,9]
+lists = binary_search(my_list,7)  
+print(lists)
