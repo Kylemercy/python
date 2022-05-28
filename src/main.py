7fd066f2ecf9755c69c81d097eae0aafcd07be38
root <root@localhost>
2022-05-28 17:05:19 +0100

    auto publish 1653753918868

--- src/main.py
+++ src/main.py
@@ -5,7 +5,15 @@ def outer_func():
   
     print("world")
   
-  return inner_fun()
+  return inner_fun
+  #lwts not execute the inner fun
 
-outer_func()
+#in other to execute the inner function
+#the outer function is assigned to a variable
+my_fun = outer_func()
+print(my_fun.__name__)
+#now my_fun is equal to inner func
+#when called it executes the inner function
+my_fun()
+
   
