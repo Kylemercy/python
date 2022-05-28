b2ee88d26351f8f720e44df578a70f8f0492a6e9
root <root@localhost>
2022-05-28 16:58:16 +0100

    auto publish 1653753496637

--- src/main.py
+++ src/main.py
@@ -1,18 +1,11 @@
-def binary_search(my_list,target):
-  low = 0
-  high = len(my_list) - 1
-  while low<= high:
-      mid = (low + high)//2
-      guess = my_list[mid]
-      if guess == target:
-        return mid
-      if mid >= target:
-        high = mid - 1
-      
-      else:
-        low =  mid +1
-  return None
-      
-my_list = [1,2,6,7,8,8,9]
-lists = binary_search(my_list,7)  
-print(lists)
+def outer_func():
+  msg = "hi"
+  def inner_fun():
+    print(msg)
+  
+    print("world")
+  
+  return inner_fun()
+
+outer_func()
+  
