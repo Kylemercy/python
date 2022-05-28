958f064a8d3d9d76b33f30e515f2a42d2fb1e7ba
root <root@localhost>
2022-05-28 17:10:23 +0100

    auto publish 1653754223117

--- src/main.py
+++ src/main.py
@@ -1,19 +1,14 @@
-def outer_func():
-  msg = "hi"
+def outer_func(message):
+  msg = message
   def inner_fun():
     print(msg)
-  
-    print("world")
   
   return inner_fun
   #lwts not execute the inner fun
 
-#in other to execute the inner function
-#the outer function is assigned to a variable
-my_fun = outer_func()
-print(my_fun.__name__)
-#now my_fun is equal to inner func
-#when called it executes the inner function
-my_fun()
+#cloures
 
-  
+my_fun = outer_func("hi")
+my_hello = outer_func("hello")
+my_fun()
+my_hello()
