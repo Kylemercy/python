7304ffa2f7c31e885ab62dc7b173e9820b2f656a
root <root@localhost>
2022-06-03 09:55:44 +0100

    auto publish 1654246544146

--- src/main.py
+++ src/main.py
@@ -1,14 +1,30 @@
-def outer_func(message):
-  msg = message
-  def inner_fun():
-    print(msg)
+def findsmall(arr):
+  smallest = arr[0]
+  small_index = 0
+  for i in range (0,len(arr)):
+    if arr[i] < smallest:
+      smallest = arr[i]
+      small_index = i
+  return small_index
+#this return the imdex of the smallest number
   
-  return inner_fun
-  #lwts not execute the inner fun
+def find_sort(arr):
+  new_arr = []
+  for i in range (len(arr)):
+    smallest = findsmall(arr)#index of the smallest
+   # number
+    new_arr.append(arr.pop(smallest))
+    #this remove the smallest number
+# which is at position 0
+#this continues until all the numbers jave been
+#removed
+  return new_arr
 
-#cloures
+sort = find_sort([3,5,21,7,2,18,9])
+check = findsmall([3,5,21,7,2,18,1,9])
+print(check)
+#this print the index position of smallest num
+print(sort)
 
-my_fun = outer_func("hi")
-my_hello = outer_func("hello")
-my_fun()
-my_hello()
+    
+#slow sort algorithm 
