faa54ae8572ee35f10e34fe02c6bd46c19da9f20
root <root@localhost>
2022-06-04 01:49:02 +0100

    auto publish 1654303742453

--- src/main.py
+++ src/main.py
@@ -1,30 +1,34 @@
-def findsmall(arr):
-  smallest = arr[0]
-  small_index = 0
-  for i in range (0,len(arr)):
-    if arr[i] < smallest:
-      smallest = arr[i]
-      small_index = i
-  return small_index
-#this return the imdex of the smallest number
+def rotateArray(a,d):
+  temp = []
+  n = len(a)
+  for i in range(d,n):
+    temp.append(a[i])
+  i = 0
+  for i in range(0,d):
+    temp.append(a[i])
+  return temp
+
+
+ 
+arr = [1, 2,9, 3, 4, 5, 6, 7]
+print("Array after left rotation is: ", end = '>= ')
+print(rotateArray(arr, 3))
+
+
+#rotating a code in left direction
+def rotateArray(a,d):
+    temp = []
+    #first create an empty list
+    n=len(a)
+    for i in range(d,n):
+        temp.append(a[i])
+    i = 0
+    for i in range (0,d):
+        temp.append(a[i])
+    a=temp.copy()
+    return a
+ 
+arr = [1, 2,9, 3, 4, 5, 6, 7]
+print("Array after left rotation is: ", end=' ')
+print(rotateArray(arr, 2))
   
-def find_sort(arr):
-  new_arr = []
-  for i in range (len(arr)):
-    smallest = findsmall(arr)#index of the smallest
-   # number
-    new_arr.append(arr.pop(smallest))
-    #this remove the smallest number
-# which is at position 0
-#this continues until all the numbers jave been
-#removed
-  return new_arr
-
-sort = find_sort([3,5,21,7,2,18,9])
-check = findsmall([3,5,21,7,2,18,1,9])
-print(check)
-#this print the index position of smallest num
-print(sort)
-
-    
-#slow sort algorithm 
