99151c24a69346280b4bd81a98f8fbb18980d843
root <root@localhost>
2022-06-07 01:40:50 +0100

    auto publish 1654562450398

--- src/main.py
+++ src/main.py
@@ -1,34 +1,34 @@
-def rotateArray(a,d):
-  temp = []
-  n = len(a)
-  for i in range(d,n):
-    temp.append(a[i])
-  i = 0
-  for i in range(0,d):
-    temp.append(a[i])
-  return temp
+def reversearray(arr,start, end):
+  while (start< end):
+    temp = arr[start]
+    arr[start] = arr[end]
+    arr[end] = temp
+    start += 1
+    end -= 1
+    
+
+def rotate(arr,d):
+  if d == 0:
+    return
+  n = len(arr)
+  d = d %n
+  reversearray(arr,0,d-1)
+  reversearray(arr,d,n-1)
+  reversearray(arr,0,n-1)
+
+def printarray(arr):
+  for i in range (0,len(arr)):
+    print(arr[i],end = " ")
 
 
+arr = [1, 2, 3, 4, 5, 6, 7]
+n = len(arr)
+d = 2
  
-arr = [1, 2,9, 3, 4, 5, 6, 7]
-print("Array after left rotation is: ", end = '>= ')
-print(rotateArray(arr, 3))
+rotate(arr, 2)  # Rotate array by 2
+printarray(arr)
+#reversal algorithm
 
+  
 
-#rotating a code in left direction
-def rotateArray(a,d):
-    temp = []
-    #first create an empty list
-    n=len(a)
-    for i in range(d,n):
-        temp.append(a[i])
-    i = 0
-    for i in range (0,d):
-        temp.append(a[i])
-    a=temp.copy()
-    return a
- 
-arr = [1, 2,9, 3, 4, 5, 6, 7]
-print("Array after left rotation is: ", end=' ')
-print(rotateArray(arr, 2))
   
