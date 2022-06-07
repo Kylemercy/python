1209aaef21a62824d23267dcc2e6681b542430b0
root <root@localhost>
2022-06-07 12:41:29 +0100

    auto publish 1654602089541

--- src/main.py
+++ src/main.py
@@ -1,21 +1,40 @@
-def rotate(arr, n):
-    x = arr[n - 1]
+maximum sum rotation
+import sys
+#also can use math module
+ 
+# Returns maximum value of i * arr[i]
+def maxSum(arr, n):
+ 
+    # Initialize result
+    res = -sys.maxsize
+ #use to return the max num in feom a list
+#and storie it in bitwise form
+#the -sign converts it to a interger
+ 
+    # Consider rotation beginning with i
+    # for all possible values of i.
+    for i in range(0, n):
+ 
+ 
+        # Initialize sum of current rotation
+        curr_sum = 0
      
-    for i in range(n - 1, 0, -1):
-        arr[i] = arr[i - 1];
+        # Compute sum of all values. We don't
+        # actually rotation the array, but
+        # compute sum by finding indexes when
+        # arr[i] is first element
+        for j in range(0, n):
          
-    arr[0] = x;
+            index = int((i + j)% n)
+            curr_sum += j * arr[index]
+     
  
+        # Update result if required
+        res = max(res, curr_sum)
+    return res
  
-# Driver function
-arr= [1, 2, 3, 4, 5]
+# Driver code
+arr = [8, 3, 1, 2]
 n = len(arr)
-
-rotate(arr, n)
  
-print ("\nRotated array is")
-for i in range(0, n):
-    print (arr[i],end = " ")
-    
-    
-#rotating an array in a clockwise direction 
+print(maxSum(arr, n))
