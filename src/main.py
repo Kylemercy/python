44ffe736e1b8076c14971259333e424920875204
root <root@localhost>
2022-06-09 13:39:17 +0100

    auto publish 1654778357555

--- src/main.py
+++ src/main.py
@@ -1,25 +1,18 @@
 #creating an array
 import array as array
-arr = array.array('i',[1,2,3,4,5,6])
-for i in range (0,len(arr)):
-  print(arr[i],end = " ")
-arr.append(8)
-print ("\n")
-for i in range (0,len(arr)):
-  print(arr[i],end = " ")
-arr.insert(4,9)
-print ("\n")
-for i in range (0,len(arr)):
-  print(arr[i],end = " ")
-  #adding elements in array
-#this is done using insert and append
-#updating an element in an array
-
-new_array = array.array('i',[1,2,2,4,5,6])
-#this is uae to cahnge an element in an array
-#exaple 2 repeats itself it can b changed
-# this is done using its index
-new_array[2] = 8
+#slicing an array
+x = list(range(1,100,3))
+#we have 33 elements here
+arr = array.array('i',x)
+for i in range(1,len(x)):
+  print(arr[i],end= " ")
 print("\n")
-for i in range (0,len(new_array)):
-  print(new_array[i],end = " ")
+arr0 = arr[10:20]
+for i in range(1,len(arr0)):
+  print(arr0[i],end = " ")
+# revising a list using slicing
+print("\n")
+arr1 = arr[::-1]
+for i in range(1,len(arr1)):
+  print(arr1[i],end = " ")
+ #revising the main list 
