2a06a8f502d944ce8bf5ab4c94574176260c9b76
root <root@localhost>
2022-06-09 12:23:12 +0100

    auto publish 1654773792701

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-import array as arr
-arr = arr.array('i',[1,2,3,4,5])
-arr.append(8)
-arr.insert(2,7)
-arr.remove(8)
+import array as array
+arr = array.array('i',[1,2,3,4,5])
+arr1 = array.array('d',[2.0,3.0,5.0,6.0,8.2])
+arr2 = array.array('u',['a','b','c','d'])
 print(arr)
+print(arr1)
+print(arr2)
