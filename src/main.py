0ce9451cc07d35f284b038f9ce57998ecdc59e8c
root <root@localhost>
2022-06-09 13:26:49 +0100

    auto publish 1654777609558

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
 #creating an array
 import array as array
-# accessing an array
-x = list(range(1,100,2))
-#this only print 50 elements
-new_array = array.array('i',x)
-for i in range(0,len(x)):
-  print(new_array[i],end = ",")
-#acessing the position of an element using its
-#index
+arr = array.array('i',[1,2,3,4,5,6])
+for i in range (0,len(arr)):
+  print(arr[i],end = " ")
+arr.append(8)
+print ("\n")
+for i in range (0,len(arr)):
+  print(arr[i],end = " ")
+arr.insert(4,9)
+print ("\n")
+for i in range (0,len(arr)):
+  print(arr[i],end = " ")
+  #adding elements in array
+#this is done using insert and append
+#updating an element in an array
+
+new_array = array.array('i',[1,2,2,4,5,6])
+#this is uae to cahnge an element in an array
+#exaple 2 repeats itself it can b changed
+# this is done using its index
+new_array[2] = 8
 print("\n")
-print(new_array[28])
+for i in range (0,len(new_array)):
+  print(new_array[i],end = " ")
