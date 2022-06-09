f95c62195834db4700017ad01671cf358f8d092c
root <root@localhost>
2022-06-09 12:50:16 +0100

    auto publish 1654775416460

--- src/main.py
+++ src/main.py
@@ -1,20 +1,12 @@
 #creating an array
 import array as array
-arr = array.array('i',[1,2,3,4,5])
-arr1 = array.array('d',[2.0,3.0,5.0,6.0,8.2])
-arr2 = array.array('u',['a','b','c','d'])
-print(arr)
-print(arr1)
-print(arr2)
-#looping through an array
-for i in range(0, len(arr)):
-  print(arr[i],end = " ")
+# accessing an array
+x = list(range(1,100,2))
+#this only print 50 elements
+new_array = array.array('i',x)
+for i in range(0,len(x)):
+  print(new_array[i],end = ",")
+#acessing the position of an element using its
+#index
 print("\n")
-
-for d in range(0, len(arr1)):
-  print(arr1[d],end = " ")
-print("\n")
-  
-for u in range(0, len(arr2)):
-  print(arr2[u],end = " ")
-print("\n")
+print(new_array[28])
