0541030bf681e668edfe9f71f67b1d27a7654fa4
root <root@localhost>
2022-06-11 16:21:59 +0100

    auto publish 1654960918763

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
-#creating an array
-import array as array
-#slicing an array
-x = list(range(1,100,3))
-#we have 33 elements here
-arr = array.array('i',x)
-for i in range(1,len(x)):
-  print(arr[i],end= " ")
-print("\n")
-arr0 = arr[10:20]
-for i in range(1,len(arr0)):
-  print(arr0[i],end = " ")
-# revising a list using slicing
-print("\n")
-arr1 = arr[::-1]
-for i in range(1,len(arr1)):
-  print(arr1[i],end = " ")
- #revising the main list 
+#linear search
+def search(arr,n,d):
+  for i in range(0,n):
+    if arr[i] == d:
+      return  i
+#this returns the index of i
+  return -1
+
+
+arr = [1,2,5,6,8,9]
+n = len(arr)
+d = 10
+res = search(arr,n,d)
+if (res == -1):
+  print("Element not found")
+else:
+  print("Element found at index ",res)
+
+
+    
+  
