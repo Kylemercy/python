560203b181202388b748d75881f48df3cd6408c4
root <root@localhost>
2022-06-15 12:36:56 +0100

    auto publish 1655293016088

--- src/main.py
+++ src/main.py
@@ -1,36 +1,32 @@
-#binary search
-def search(arr,n,):
-  left = 0#(the first index)
-  #low
-  right = len(arr) - 1#last index
-  # high
+#checking of two words are anagram
+''' One string is an anagram of another
+ if the second is simply a rearrangement
+ of the first. For example,
+  'heart' and 'earth' are anagram   '''
+def anagram(s1,s2):
+  a_list1 = list(s1)
+  a_list2 = list(s2)
   
-  while left <= right:
-    mid = (left + right)// 2
+  a_list1.sort()
+  a_list2.sort()
+# here the list i already sorted out'''  
+  matches = True
+  pos = 0
   
-    if arr[mid] == n:
-      return mid
+  while pos < len(s1) and matches:
+    if a_list1[pos] == a_list2[pos]:
+      pos += 1
     else:
-      if arr[mid] < n:
-        left = mid + 1
-        # here the left index changes from
-# zero to mid +1 that is the mid value + 1
-# thereby reducing the list
-      else:
-        right = mid - 1
-        #here the mid value is greater than n
-# there fore the len of the high is now reduced to
-# mid value -1
-   
-  return -1
-arr = [1,2,5,6,8,9,20,67,71,76,80]
-n = 76 
-res = search(arr,n,)
-if (res == -1):
-  print("Element not found")
-else:
-  print("Element found at index ",res)
+      matches = False
+   #therefore the loops break
+  return "Not an anagram."
 
 
+word = anagram('abcde','ecad')
+print(word)
+    
+# writing an anagram using sorting and comparing m
+#method   
+  
     
   
