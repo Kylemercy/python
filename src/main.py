5c232a14fad34e00db69dfcbba201238d05b0ca6
root <root@localhost>
2022-06-16 02:04:52 +0100

    auto publish 1655341492718

--- src/main.py
+++ src/main.py
@@ -1,8 +1,37 @@
-stocks = ['reliance', 'infosys', 'tcs']
-
-prices = [2175, 1127, 2750]
+# Python code to demonstrate the working of
+# unzip
  
+# initializing lists
 
-new_dict = zip(stocks, prices)
+name = ["Manjeet", "Nikhil", "Shambhavi", "Astha"]
 
-print(dict(new_dict))
+roll_no = [4, 1, 3, 2]
+
+marks = [40, 50, 60, 70]
+ 
+# using zip() to map values
+
+mapped = zip(name, roll_no, marks)
+# converting values to print as list
+mapped = list(mapped)
+ 
+# printing resultant values
+
+print("The zipped result is : ", end="")
+
+print(mapped)
+print("\n")
+ 
+# unzipping values
+
+namz, roll_noz, marksz = zip(*mapped)
+print("The unzipped result: \n", end="")
+ 
+# printing initial lists
+
+print("The name list is : ", end="")
+print(namz)
+print("The roll_no list is : ", end="")
+print(roll_noz)
+print("The marks list is : ", end="")
+print(marksz)
