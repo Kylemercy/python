73fd9cc91cbba089064b2bec952a0090433a2987
root <root@localhost>
2022-06-16 02:11:56 +0100

    auto publish 1655341916589

--- src/main.py
+++ src/main.py
@@ -1,37 +1,16 @@
-# Python code to demonstrate the working of
-# unzip
+
+# Python code to demonstrate the application of
+# zip()
  
-# initializing lists
+# initializing list of players.
 
-name = ["Manjeet", "Nikhil", "Shambhavi", "Astha"]
-
-roll_no = [4, 1, 3, 2]
-
-marks = [40, 50, 60, 70]
+players = ["Sachin", "Sehwag", "Gambhir", "Dravid", "Raina"]
  
-# using zip() to map values
+# initializing their scores
 
-mapped = zip(name, roll_no, marks)
-# converting values to print as list
-mapped = list(mapped)
+scores = [100, 15, 17, 28, 43]
  
-# printing resultant values
+# printing players and scores.
 
-print("The zipped result is : ", end="")
-
-print(mapped)
-print("\n")
- 
-# unzipping values
-
-namz, roll_noz, marksz = zip(*mapped)
-print("The unzipped result: \n", end="")
- 
-# printing initial lists
-
-print("The name list is : ", end="")
-print(namz)
-print("The roll_no list is : ", end="")
-print(roll_noz)
-print("The marks list is : ", end="")
-print(marksz)
+for pl, sc in zip(players, scores):
+  print(f"player: {pl}, scores : {sc}")
