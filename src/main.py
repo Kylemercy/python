7fb377565a9859f1e7823bb140abf14148492eaa
root <root@localhost>
2022-06-16 15:26:52 +0100

    auto publish 1655389611903

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from itertools import zip_longest
-L1 = [1,2,3,4,5]
-L2 = ['a','b','c','d']
-
-zipL_L1L2 = zip_longest(L1,L2,)
-
-print(list(zipL_L1L2))
-''' Notice how the item 5 from
- L1 is still included.
-  But as there's no matching item in L2, 
-  the second element in the last tuple is None'''
+'''How to print single quote or
+ double quote on screen?'''
+print("Hi Mr Geek.")
+  
+# use of escape sequence
+print("He said, \"Welcome to GeeksforGeeks\"")    
+  
+print('Hey so happy to be here')
+  
+# use of mix quotes
+print ('Getting Geeky, "Loving it"')                
