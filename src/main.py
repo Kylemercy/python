18bb6ccf5fb980f5c50f5803aebca61f87689d76
root <root@localhost>
2022-06-16 02:00:53 +0100

    auto publish 1655341253010

--- src/main.py
+++ src/main.py
@@ -1,30 +1,8 @@
-#zip
-# unpacking of zip
-coordinate = ['x', 'y', 'z']
-value = [3, 4, 5]
+stocks = ['reliance', 'infosys', 'tcs']
 
-result = zip(coordinate, value)
-result_list = list(result)
-print(result_list)
+prices = [2175, 1127, 2750]
+ 
 
-c, v =  zip(*result_list)
-print('c =', c)
-print('v =', v)
+new_dict = zip(stocks, prices)
 
-numbersList = [1, 2, 3]
-str_list = ['one', 'two']
-numbers_tuple = ('ONE', 'TWO', 'THREE', 'FOUR')
-
-# Notice, the size of numbersList
-# and numbers_tuple is different
-result = zip(numbersList, numbers_tuple)
-
-# Converting to set
-result_set = set(result)
-print(result_set)
-
-result = zip(numbersList, str_list, numbers_tuple)
-
-# Converting to set
-result_set = set(result)
-print(result_set)
+print(dict(new_dict))
