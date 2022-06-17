233e7c88cd71e7096ff28f9b747018c5348d3486
root <root@localhost>
2022-06-17 13:51:28 +0100

    auto publish 1655470288510

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
-from string import ascii_lowercase as alpha_lower
-def pangram(s):
-  return set(alpha_lower) - set(s.lower()) == set([])
-s = input("Enter a sentence: ")
-if pangram(s) == True:
-    print("The string is a pangram")
-else:
-    print("the string is not a pangram")
+from string import ascii_lowercase as asc_lower
+def check_pangram(s):
   
-#checking a pangram using set
+  res = [ ]
+  val = set(asc_lower) - set(s.lower())
+  for i in val:
+    res .append(i)
+  return res
+  
+
+ 
+s = input("Enter sentence: ").lower()
+txt = check_pangram(s)
+print(txt)
+  
+#a pangram is a string that contains all the
+# character of the alphabet 
