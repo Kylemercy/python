9e6819e925cdf42224913e4cda7e907cdf901e3c
root <root@localhost>
2022-06-19 21:30:57 +0100

    auto publish 1655670657193

--- src/main.py
+++ src/main.py
@@ -1,47 +1,24 @@
-'''Category                No of letters unmatched
-Pangram                     0
-Lipogram                    >1
-Pangrammatic Lipogram       1
-'''
-# function to check for Pangrammatic Lipogram
-def panLipogramChecker(s):
- 
-    # dictionary to keep count of the
-    # occurrence of each letter
-    counter = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0,
-               'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
-               'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0,
-               'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
-               'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0,
-               'z': 0}
- 
-    s = s.lower()
- 
-    # increment count of characters in dictionary
-    for i in s:
-        if (i.isalpha()):
-            counter[i] += 1
- 
-    # returns a list containing the values of all
-    # the keys in h=the dictionary
-    b = list(counter.values())
- 
-    if (b.count(0) > 1):
-        print("Not a pangram, but might be a lipogram.")
-    elif (b.count(0) == 1):
-        print("Pangrammatic Lipogram.")
-    elif (b.count(0) < 1):
-        print("Pangram.")
- # isalpha() is a string method that checks if 
- # a character is an alphabet
-def main():
-    panLipogramChecker("The quick brown fox \
-                        jumped over the lazy dog")
-    panLipogramChecker("The quick brown fox \
-                        jumps over the lazy dog")
-    panLipogramChecker("The quick brown fox \
-                        jum over the lazy dog")
- 
- 
-if __name__ == '__main__':
-    main()
+# creating a singly linked lists
+class linked_list:
+
+  def __init__(self, data, nextnode = None):
+    self.data = data
+    self.nextnode = nextnode
+  
+
+n1 = linked_list("fish")
+n2 = linked_list("egg")
+n3 = linked_list("bread")
+n4 = linked_list("butter")
+n1.nextnode = n2
+n2.nextnode = n3
+#adding a new item
+n3.nextnode = n4
+current_node = n1
+while current_node:#or use while true
+  print(current_node.data,end = " > ")
+  if current_node.nextnode == None:
+    print("None")
+    break
+  current_node = current_node.nextnode
+#butter is then added to the list 
