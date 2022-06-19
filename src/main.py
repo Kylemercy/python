037c82d4f91f2994f9a48f5ec5e24d608ba80b27
root <root@localhost>
2022-06-19 22:42:06 +0100

    auto publish 1655674925750

--- src/main.py
+++ src/main.py
@@ -1,24 +1,48 @@
-# creating a singly linked lists
-class linked_list:
+'''using another method of creating a singly
+ linked lists'''
+class linked:
 
-  def __init__(self, data, nextnode = None):
-    self.data = data
-    self.nextnode = nextnode
+  def __init__(self, value, nextnode = None):
+    self.value = value
+    self.nextnode = nextnode  
+class single_list:
+  def __init__(self, head = None):
+    #this help keep track of the head of the list
+    self.head = head
   
+  def insert(self, value):
+      node = linked(value)
+      
+      if self.head is None:
+        self.head = node
+        return
+      current_node =  self.head
+        # value i the element we want to insert
+  
+    # we create a node with the value we
+#want to insert     
+#here it checks if the node is none,if so the
+#the value inserted becomes our head     
+      while True:
+        if current_node.nextnode is None:
+          current_node.nextnode = node
+          break
+        current_node = current_node.nextnode
+# this traverse through the list if incase
+#when the node points to a none value we 
+#are at the till then node is then inserted there
+        
+  def printlist(self):
+     current_node = self.head
+     while current_node is not None:
+       print(current_node.value,end =  " > ")
+       current_node = current_node.nextnode
+     
+     print("None")
 
-n1 = linked_list("fish")
-n2 = linked_list("egg")
-n3 = linked_list("bread")
-n4 = linked_list("butter")
-n1.nextnode = n2
-n2.nextnode = n3
-#adding a new item
-n3.nextnode = n4
-current_node = n1
-while current_node:#or use while true
-  print(current_node.data,end = " > ")
-  if current_node.nextnode == None:
-    print("None")
-    break
-  current_node = current_node.nextnode
-#butter is then added to the list 
+ll = single_list()
+ll.printlist()
+ll.insert(8)
+ll.printlist()
+ll.insert(3)
+ll.printlist()
