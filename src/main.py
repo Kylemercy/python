b698c8f1766b7dc63c4c46329d8174bb920b230f
root <root@localhost>
2022-06-20 17:37:01 +0100

    auto publish 1655743021459

--- src/main.py
+++ src/main.py
@@ -39,8 +39,28 @@ class linked:
     curr_node = self.head
     while True:
       curr_node = curr_node.next
-      if cur_index == index:return curr_node.data
+      if cur_index == index:
+        return curr_node.data
+      
       cur_index += 1
+  def erase(self,index):
+    if index >= self.length():
+      print("Error 'Get' index out of range")
+      return# this returns all the items
+      # after raising an error
+    cur_indx = 0
+    cur_node = self.head
+    while True:
+      last_node = cur_node
+    
+      cur_node = cur_node.next
+      if cur_indx == index:
+        
+         last_node.next = cur_node.next
+         return
+      
+      cur_indx += 1
+  
 n1= linked()
 n1.display()
 n1.append("egg")
@@ -50,8 +70,10 @@ n1.append("milk")
 n1.display()
 print( n1.length())
 print(n1.get(2) ) 
-  
-
+n1.erase(2)
+n1.display()
+n1.erase(5)
+n1.display()
   
 
     
