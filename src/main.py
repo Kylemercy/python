86fd148a0b598c95569d3187429d2de24b45c411
root <root@localhost>
2022-06-29 01:25:11 +0100

    auto publish 1656462311804

--- src/main.py
+++ src/main.py
@@ -1,24 +1,14 @@
-def arr_reverse(arr,n):
-  first = 0
-  n = 5
-  end = n- 1
-  while (first < end):
-     temp = arr[first]
-     arr[first] = arr[end]
-     arr[end] = temp
-     first += 1
-     end -= 1
-  return arr
-
+#recursion
+def russian_doll(doll):
+  if doll == 1:
+    print("All the dolls are opened")
+    return
   
-arr = [2,45,6,8,9]  
-ti = arr_reverse(arr,5)
-print(ti)
-'''in the following code, we have assigned the 
-value of index 1(start)to the temp variable
- and end index to start index 1. 
- Then we have assigned the value of 
- temp (stored value of index 1 ie 6) to end.
- we then increase start by 1 and decrease end
-by 1'''
- 
+  else:
+    russian_doll(doll-1)
+    return None
+
+russian_doll(1)
+   
+  
+  
