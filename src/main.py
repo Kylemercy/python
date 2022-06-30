f8ecef8b5f89f28d9aeab1c1492d1d35c7f44dda
root <root@localhost>
2022-06-30 12:36:56 +0100

    auto publish 1656589016080

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-def fibanocci(n):
+def fibonacci(n):
   assert n >= 0 and n == int(n)," invalid input"
 
   if n in(0,1):
     return n
   else:
-    return fibanocci(n-1) + fibanocci(n - 2)
+    return fibonacci(n-1) + fibonacci(n - 2)
   
   
-print(fibanocci(-1))
+print(fibonacci(7))
+#recursive Fibonacci 
