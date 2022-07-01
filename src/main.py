8010c821c6ae5f41ae37bbd23559c64294c49c1b
root <root@localhost>
2022-07-01 09:21:48 +0100

    auto publish 1656663708524

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
-def sum_digit(n):
-  if n < 0:
-    return ("Number must be a positive number")
+# finding the greatest common divisor of two numbers
+# finding gcd using elucidean algorithm
+def gcd(x,y):
+  assert int(x) == x and int(y) == y,"Number must be integers only"
+  if x < 0:
+    x = -1 * x
+  if y < 0:
+    y = -1 * y
   
-  if n == 0:
-    return 0
+  if y == 0:
+    return x
+  
   
   else:
-    return int(n%10) + sum_digit(int(n/10))
+    return gcd(y,x%y)
 
-print(sum_digit(-5))
+print(gcd(45.0,-3.6))
   
   
+  
+
   
