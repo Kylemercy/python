9fd12f298d33a4288075ba9427d008c991ccb72e
root <root@localhost>
2022-07-04 17:00:23 +0100

    auto publish 1656950423257

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
-#power of a number
-def power(base,exp):
-  assert int(xp) ==xp and xp < 0,"exponent must be a positive integer"
-  if exp == 0:
-    return 1
-  if exp == 1:
-    return base
-  else:
-    return base * power(base,exp -1)
-  
-print(power(2,7))
-'''note a each recursive call our exponent(exp)
-keeps o decreasing until exp becomes 1, then it 
-return te base '''
-  
+def find_maxnum(n):
+  biggest_num = array[0]
+  for num in range(1,len(array)):
+    if array[num] > biggest_num:
+      biggest_num = array[num]
+  print(biggest_num)
+
+
+array = [3,4,1,9,11,163,6,18]
+find_maxnum(array)
+    
   
