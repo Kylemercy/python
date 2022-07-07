fb073cfbd78d9f4abe593a396d280ffeca11917d
root <root@localhost>
2022-07-07 17:49:38 +0100

    auto publish 1657212577816

--- src/main.py
+++ src/main.py
@@ -1,38 +1,39 @@
+#operations in an array
 from array import *
-arr1 = array('i',[2,4,5,7,8,10])
-arr2 = array('d',[1.3,1.7,8.9,3.4])
-arr3 = array('u',['a','b','f','g','k'])
-arr2.insert(0,2.3)
-arr3.insert(1,'e')
-print(arr2)
-print(arr3)
-arr1.insert(3,13)
-arr1.insert(0,0)
-
-print(arr1)
-#travasal of array
-def travasal(array):
-  for i in array:
-    print(i)
-travasal(arr1)
-print('\n','accessing elements in an array')
-def accessing(array,index):
-  if index > len(array):
-    print('index i out of range')
-  print(array[index])
-
-accessing(arr1,5)
-#accessing(arr1,10)
+#* import everything in array
+my_array = array('i',[1,2,5,5,6,7,8,9])
+#travasal
+for i in my_array:
+  print(i)
+my_array.append(5)
+print(my_array)
+#ascessing element using index
 print('\n')
-def searching(array, value):
-  for i in array:
-    if i == value:
-      return array.index(value)
-  return ("the element does not exist in the array")
-print(searching(arr1,5) )
-print("searching returns the index of the value")
-# deletion of elements in an array using remove
-arr2.remove(1.7)
-print(arr2)
+print(my_array[2])
+#inserting using insert method
+my_array.insert(3,11)
+print(my_array)
+#using extend method
+my_array2 = array('i',[20,21,23])
+my_array.extend(my_array2)
+print(my_array)
+#addimg list to an array
+my_list = [56,72]
+my_array.fromlist(my_list)
+print(my_array)
+#using remove func
+my_array.remove(72)
+print(my_array)
+#note remove only remove the first occursnce of
+#an element 
+my_array.pop()
+print(my_array)
+#reversing an array
+print('\n')
+my_array.reverse()
+print(my_array)
+print(my_array.buffer_info())
+#shows number of element in the array
+#count
+print(my_array.count(5) )
 
-  
